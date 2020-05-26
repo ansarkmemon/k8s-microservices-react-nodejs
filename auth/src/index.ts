86117bbf4ea59149bc1bf7961e3944b12763d767
Ansar Memon <ansar.memon@hotmail.com>
2020-05-26 12:34:39 +0400

    Refactoring to prepare for tests

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,36 +1,5 @@
-import { NotFoundError } from './errors/not-found-error';
-import express from 'express';
-import cookieSession from 'cookie-session';
-import 'express-async-errors';
 import mongoose  from 'mongoose';
-import { json } from 'body-parser';
-
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signupRouter } from './routes/signup';
-import { signoutRouter } from './routes/signout';
-
-import { errorHandler } from './middlewares/error-handler';
-
-const app = express();
-app.set('trust proxy', true);
-
-app.use(json());
-app.use(cookieSession({
-  signed: false,
-  secure: true
-}))
-
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signupRouter);
-app.use(signoutRouter);
-
-app.all('*', async () => {
-  throw new NotFoundError();
-});
-
-app.use(errorHandler);
+import { app } from './app';
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
